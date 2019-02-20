bf0537b45dd4e3f6156c7ebb42faabada0707402
Samuel LACAILLE <samuel76560@outlook.fr>
2019-02-20 11:25:28 +0100

    Si non loggé : Menu classique
    Sinon : Menu sans item inscription

--- includes/header.php
+++ includes/header.php
@@ -2,19 +2,24 @@
 <header>
     <nav>
         <ul>
-            <li><a href="index.php?page=accueil">Accueil</a></li>
-            <li><a href="index.php?page=produits">Produits</a></li>
-            <li><a href="index.php?page=inscription">Inscription</a></li>
             <?php
             if (isset($_SESSION['login']) && $_SESSION['login'] == 1) {
-                echo "<li><a href=\"index.php?page=logout\">Logout</a></li>";
+                // echo "<li><a href=\"index.php?page=logout\">Logout</a></li>";
+                include_once "./includes/headerLogin.php";
+
             }
 
             else {
-                echo "<li><a href=\"index.php?page=login\">Login</a></li>";
+
+               echo" <li><a href=\"index.php?page=accueil\">Accueil</a></li>";
+               echo "<li><a href=\"index.php?page=produits\">Produits</a></li>";
+               echo "<li><a href=\"index.php?page=login\">Login</a></li>";
+               echo "<li><a href=\"index.php?page=inscription\">Inscription</a></li>";
+
+           echo " <li><a href=\"index.php?page=contact\">Contact</a></li>";
             }
             ?>
-            <li><a href="index.php?page=contact">Contact</a></li>
+
         </ul>
     </nav>
 </header>
