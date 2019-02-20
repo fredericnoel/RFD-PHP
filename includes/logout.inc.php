b6025665c742faa0f0deaca03cedfd4c7168c50c
Samuel LACAILLE <samuel76560@outlook.fr>
2019-02-20 10:34:02 +0100

    changement url logout

--- includes/logout.inc.php
+++ includes/logout.inc.php
@@ -1,4 +1,4 @@
 <?php
 $_SESSION['login'] = 0;
 session_destroy();
-echo "<script>document.location.href='http://localhost/PHP1/'</script>";
+echo "<script>document.location.href='http://localhost/rfdphp/index.php'</script>";
