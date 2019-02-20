90a67c8c1d279b02cd433c3ced2ba36cd15cb288
David Claassen <17654@csmrouen.net>
2019-02-20 12:04:57 +0100

    Ajout Formulaire contact

--- includes/contact.inc.php
+++ includes/contact.inc.php
@@ -1,7 +1,14 @@
 <h1>Contact</h1>
 
 <?php
+if (isset($_SESSION['login']) && $_SESSION['login'] == 1) {
+    if (isset($_SESSION['prenom']) && isset($_SESSION['nom'])) {
 
-require_once "frmContact.php";
+        $nom = $_SESSION['nom'];
+        $prenom = $_SESSION['prenom'];
+        $mail = $_SESSION['mail'];
+    }
 
+    require_once "frmContact.php";
+}
 ?>
