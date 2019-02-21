4a6d80d9015d37536217d64a94e75bbeaa786ffd
Frédéric NOËL <contact@fredericnoel.com>
2019-02-21 09:25:06 +0100

    Test session pour affichage login/logout

--- includes/header.php
+++ includes/header.php
@@ -1,5 +1,18 @@
+<?php
+if (isset($_SESSION['login']) && $_SESSION['login'] == 1) {
+    if (isset($_SESSION['prenom']) && isset($_SESSION['nom'])) {
+        $bonjour = $_SESSION['prenom'] . " " . $_SESSION['nom'];
+    }
+
+}
+
+else {
+    unset($bonjour);
+}
+?>
 <body>
 <header>
+    <?php if(isset($bonjour)) echo "<p>Bonjour $bonjour</p>"; ?>
     <nav>
         <ul>
             <li><a href="index.php?page=accueil">Accueil</a></li>
