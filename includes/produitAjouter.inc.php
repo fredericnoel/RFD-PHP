b32a1ab5e73deb6a4f56d4614cfc39efa711654e
Frédéric NOËL <contact@fredericnoel.com>
2019-02-21 12:45:00 +0100

    Récupération données $_FILES

--- includes/produitAjouter.inc.php
+++ includes/produitAjouter.inc.php
@@ -5,9 +5,15 @@ if (isset($_POST['albator'])) {
     $reference = isset($_POST['reference']) ? $_POST['reference'] : "";
     $nomProduit = isset($_POST['nomProduit']) ? $_POST['nomProduit'] : "";
     $prix = isset($_POST['prix']) ? $_POST['prix'] : "";
-    $photo = $_FILES['photo'];
+    //$photo = $_FILES['photo'];
+    $nomPhoto = $_FILES['photo']['name'];
+    $typePhoto = $_FILES['photo']['type'];
+    $tmpPhoto = $_FILES['photo']['tmp_name'];
+    $errorPhoto = $_FILES['photo']['error'];
+    $sizePhoto = $_FILES['photo']['size'];
 
-    var_dump($photo);
+
+
 }
 
 else {
