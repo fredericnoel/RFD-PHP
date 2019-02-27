6f88de329bb24f99bedaa67a9e13fb0bd0a3650c
Frédéric NOËL <contact@fredericnoel.com>
2019-02-27 10:30:42 +0100

    Test erreur upload. Remplacement des espaces par des _. Fonction strrt() pour remplacement des accents.

--- includes/produitAjouter.inc.php
+++ includes/produitAjouter.inc.php
@@ -5,13 +5,23 @@ if (isset($_POST['albator'])) {
     $reference = isset($_POST['reference']) ? $_POST['reference'] : "";
     $nomProduit = isset($_POST['nomProduit']) ? $_POST['nomProduit'] : "";
     $prix = isset($_POST['prix']) ? $_POST['prix'] : "";
-    //$photo = $_FILES['photo'];
+
     $nomPhoto = $_FILES['photo']['name'];
     $typePhoto = $_FILES['photo']['type'];
     $tmpPhoto = $_FILES['photo']['tmp_name'];
     $errorPhoto = $_FILES['photo']['error'];
     $sizePhoto = $_FILES['photo']['size'];
 
+    if ($errorPhoto == 0) {
+        $nomPhoto = str_replace(" ", "_", $nomPhoto);
+        $nomPhoto = strtr($nomPhoto,
+            'ÀÁÂÃÄÅÇÈÉÊËÌÍÎÏÒÓÔÕÖÙÚÛÜÝàáâãäåçèéêëìíîïðòóôõöùúûüýÿ',
+            'AAAAAACEEEEIIIIOOOOOUUUUYaaaaaaceeeeiiiioooooouuuuyy');
+    }
+
+    else {
+        echo "<p>Môrche pô</p>";
+    }
 
 
 }
